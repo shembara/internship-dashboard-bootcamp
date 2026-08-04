cad9bcd1ad343166786af080151a0ec941a351d5
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-08-04 12:49:22 +0300

    Added test to the utils.ts

---
+++ src/lib/utils.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+
+import { formatDate } from "./utils";
+
+describe("formatDate", () => {
+  it("formats ISO date values as MM/DD/YYYY", () => {
+    expect(formatDate("2026-08-04T00:00:00.000Z")).toBe("08/04/2026");
+    expect(formatDate("2026-08-04")).toBe("08/04/2026");
+  });
+});
